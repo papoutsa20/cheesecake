d91f7353a63328ff59db6d9312030700d5281c5b
papoutsa20 <papoutsa20@up.edu>
2020-01-26 20:38:48 -0800

    added tests with jest

---
+++ hw3/test/cheesecake.test.js
@@ -0,0 +1,29 @@
+/**
+CS 341 HW 3 Spring 2020
+by Stelios Papoutsakis
+Test that if order button is clicked,
+the table tag that has the selector and radio buttons
+**/
+
+var fs = require('fs');
+
+test('test header 1 value', () => {
+	var html = fs.readFileSync('public/index.html', 'utf8')
+	expect(html).toEqual(expect.anything());
+
+	//test 1
+	document.body.innerHTML = html;
+	const $ = require('jquery');
+	expect($('h1').html()).toBe("Cheesecake Order Form");
+
+});
+
+test('pressing order button', () => {
+	var html = fs.readFileSync('public/index.html', 'utf8')
+	expect(html).toEqual(expect.anything());
+	document.body.innerHTML = html;
+	const $ = require('jquery');
+	//test 2
+	$("#order").click();
+	expect($("table").length === 0);
+});
