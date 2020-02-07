20377a8b5daed09dfb44820eb7c304dda4963e98
papoutsa20 <papoutsa20@up.edu>
2020-02-07 11:50:46 -0800

    figured out part 1 of hw4

--- hw3/app.js
+++ hw3/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+/*Code Stelios wrote*/
+var jsonRouter = require('./routes/json');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//Code Stelios Wrote
+app.use('/orders', jsonRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ hw3/routes/json.js
@@ -0,0 +1,39 @@
+/*
+responds to /orders with a static json
+HW4 by Stelios Papoutsakis CS 341 02/07/2020
+*/
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.json({
+  data : [
+		{ 
+			"topping": "cherry",
+			"quantity": 2
+		},
+	    { 
+			"topping": "chocolate",
+			"quantity": 8
+		},
+	    { 
+			"topping": "cherry",
+			"quantity": 90
+		},
+	    { 
+			"topping": "plain",
+			"quantity": 4
+		},
+	    { 
+			"topping": "cherry",
+			"quantity": 1
+		},
+	    { 
+			"topping": "cherry",
+			"quantity": 3
+		}
+
+  ]});
+ });
+
+module.exports = router;
